ef72277773a1fa264caec442bf39e8efd8202194
Cyril Le Mat <clemat@csod.com>
2022-04-03 18:32:39 +0200

    detail type signatures in model describe

--- modelkit/utils/pretty.py
+++ modelkit/utils/pretty.py
@@ -1,8 +1,57 @@
+import inspect
+from pydantic import BaseModel
 from rich.markup import escape
 from rich.tree import Tree
+import typing
+from typing import Dict, List, Optional, Tuple, Union
+from textwrap import indent
 
 
 def pretty_print_type(typ):
+    def print_list(my_list, car_from, car_to, max_car_in_line=50):
+        inline_s = car_from + ",".join(my_list) + car_to
+        if len(inline_s) <= max_car_in_line:
+            return escape(inline_s)
+        multiline_s = car_from + "\n"
+        multiline_s += indent(",\n".join(my_list), " " * 2)
+        multiline_s += "\n" + car_to
+        return escape(multiline_s)
+
+    # For simple types
+    if type(typ) is type:
+        return escape(typ.__name__)
+
+    # For lists
+    if type(typ) is list:
+        return print_list([pretty_print_type(x) for x in typ], "[", "]")
+
+    # For tuples
+    if type(typ) is tuple:
+        return print_list([pretty_print_type(x) for x in typ], "(", ")")
+
+    # For dicts
+    if type(typ) is dict:
+        return print_list(
+            [f"{key}: " + pretty_print_type(item) for key, item in typ.items()],
+            "{",
+            "}",
+        )
+
+    # For typing types
+    if type(typ) is typing._GenericAlias:
+        s = escape(typ._name)
+        if typ.__args__:
+            a = [pretty_print_type(x) for x in typ.__args__]
+            if s:
+                s += print_list(a, "[", "]")
+            else:
+                s = print_list(a, "(", ")")
+        return s
+
+    # For pydantic classes
+    if inspect.isclass(typ) and issubclass(typ, BaseModel):
+        return escape(typ.schema_json(indent=2, by_alias=True))
+
     typ_str = str(typ)
     if "'" in typ_str:  # for class wrapper
         typ_str = typ_str.split("'")[1]
